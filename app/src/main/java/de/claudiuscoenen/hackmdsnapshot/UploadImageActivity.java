24796da0c180959f4492c7fd18f186d849f189a3
Christine Emrich <christine.emrich@web.de>
2017-12-11 21:24:50 +0100

    Create basic android project

---
+++ app/src/main/java/de/claudiuscoenen/hackmdsnapshot/UploadImageActivity.java
@@ -0,0 +1,13 @@
+package de.claudiuscoenen.hackmdsnapshot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UploadImageActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_upload_image);
+	}
+}
