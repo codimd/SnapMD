a42eea4ed7a69e6bf98547a578a028da7e8331d6
Christine Emrich <christine.emrich@web.de>
2017-12-12 10:05:56 +0100

    Load pads from remote server

--- app/src/main/java/de/claudiuscoenen/hackmdsnapshot/padselection/PadSelectionFragment.java
+++ app/src/main/java/de/claudiuscoenen/hackmdsnapshot/padselection/PadSelectionFragment.java
@@ -8,14 +8,16 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.claudiuscoenen.hackmdsnapshot.HackMdApplication;
 import de.claudiuscoenen.hackmdsnapshot.R;
 import de.claudiuscoenen.hackmdsnapshot.model.Pad;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
 
 
 public class PadSelectionFragment extends Fragment {
@@ -24,6 +26,7 @@ public class PadSelectionFragment extends Fragment {
 	RecyclerView padList;
 
 	private Listener listener;
+	private CompositeDisposable loadingOperations = new CompositeDisposable();
 
 	public PadSelectionFragment() {
 		// Required empty public constructor
@@ -34,16 +37,17 @@ public class PadSelectionFragment extends Fragment {
 		View view = inflater.inflate(R.layout.fragment_pad_selection, container, false);
 		ButterKnife.bind(this, view);
 
-		List<Pad> testPads = new ArrayList<>();
-		for (int i = 0; i < 20; i++) {
-			testPads.add(new Pad("id" + i, "Test Pad " + i));
-		}
-
 		PadListAdapter adapter = new PadListAdapter(listener::onPadSelected);
-		adapter.setPads(testPads);
-
 		padList.setLayoutManager(new LinearLayoutManager(getContext()));
 		padList.setAdapter(adapter);
+
+		HackMdApplication app = (HackMdApplication) getActivity().getApplication();
+
+		Disposable disposable = app.getApi()
+				.getPads("my@mail.de", "mypassword")
+				.observeOn(AndroidSchedulers.mainThread())
+				.subscribe(adapter::setPads, this::onLoadPadsError);
+		loadingOperations.add(disposable);
 
 		return view;
 	}
@@ -62,6 +66,16 @@ public class PadSelectionFragment extends Fragment {
 	public void onDetach() {
 		super.onDetach();
 		listener = null;
+	}
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		loadingOperations.dispose();
+	}
+
+	private void onLoadPadsError(Throwable t) {
+		Toast.makeText(getContext(), t.getMessage(), Toast.LENGTH_LONG).show();
 	}
 
 	/**
